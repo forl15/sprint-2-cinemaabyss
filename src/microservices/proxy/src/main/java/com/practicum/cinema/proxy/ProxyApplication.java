63db29a75b9a8c24d87b3c659c48c21400a4585e
Denis Antipov <d_antipov@energocomm.ru>
2026-06-15 19:01:47 +0300

    proxy

---
+++ src/microservices/proxy/src/main/java/com/practicum/cinema/proxy/ProxyApplication.java
@@ -0,0 +1,19 @@
+package com.practicum.cinema.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
